02cc2936f9fd71a1001223c6f8c372af45ae541a
Jure Vito Srovin <jure.vito@gmail.com>
2021-12-27 10:01:08 +0100

    Add health checks

--- api/health.go
+++ api/health.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -11,5 +12,26 @@ func (server *Server) Live(ctx *gin.Context) {
 }
 
 func (server *Server) Ready(ctx *gin.Context) {
+
+	// Check connection to station service.
+	url := fmt.Sprintf("%s/health/ready", server.config.StationsAddress)
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode != http.StatusOK {
+		ctx.JSON(http.StatusServiceUnavailable, gin.H{"status": "DOWN"})
+		ctx.Abort()
+		return
+	}
+	defer resp.Body.Close()
+
+	// Check connection to reservation service.
+	url = fmt.Sprintf("%s/health/ready", server.config.ReservationsAddress)
+	resp, err = http.Get(url)
+	if err != nil || resp.StatusCode != http.StatusOK {
+		ctx.JSON(http.StatusServiceUnavailable, gin.H{"status": "DOWN"})
+		ctx.Abort()
+		return
+	}
+	defer resp.Body.Close()
+
 	ctx.JSON(http.StatusOK, gin.H{"status": "UP"})
 }
